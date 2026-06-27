85cdbf5244c6081db23b5c36b907533457dc1ca1
tylnodev <fboyquangninh@gmail.om>
2026-06-27 09:28:36 +0700

    bug: fix scroll login

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -3,9 +3,11 @@ import AppToast from '@/components/ui/AppToast.vue'
 </script>
 
 <template>
-  <div class="min-h-svh bg-white flex flex-col overflow-y-auto">
+  <div class="h-svh bg-white flex flex-col overflow-hidden">
     <div class="flex-none bg-white" style="height: env(safe-area-inset-top)" />
-    <slot />
+    <div class="flex-1 overflow-y-auto overscroll-contain">
+      <slot />
+    </div>
     <AppToast />
   </div>
 </template>
